9ffc01a83dd2872c2ce4639cdddcb2453e4e9744
Teemu Viikeri <teemu.viikeri@tuni.fi>
2020-03-06 14:23:11 +0200

    Initial commit

---
+++ android/src/fi/tuni/atomics/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fi.tuni.atomics;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fi.tuni.atomics.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/fi/tuni/atomics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.tuni.atomics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.tuni.atomics.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
