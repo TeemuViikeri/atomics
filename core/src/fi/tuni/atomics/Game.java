ca4bad605b40ce0a297bd09b6da14c4e841e56e2
Teemu Viikeri <teemu.viikeri@tuni.fi>
2020-03-09 12:43:14 +0200

    Setup code

--- core/src/fi/tuni/atomics/Game.java
+++ core/src/fi/tuni/atomics/Game.java
@@ -3,31 +3,64 @@ package fi.tuni.atomics;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	// Non-initiated fields
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private TiledMap tiledMap;
+	private TiledMapRenderer tiledMapRenderer;
+
+	// Initiated fields
+	private float scale = 1/100f;
 	
 	@Override
 	public void create () {
+		// libGDX
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false);
+
+		// TiledMap
+		tiledMap = new TmxMapLoader().load("atomicsdemo.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, scale);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(camera.combined);
+		clearScreen(101/255f, 214/255f, 186/255f);
+		moveCamera(camera);
+
+		tiledMapRenderer.render();
+		tiledMapRenderer.setView(camera);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
+	}
+
+	private void moveCamera(OrthographicCamera camera) {
+		// Change camera position accordingly!!
+		camera.position.x = 0;
+		camera.position.y = 0;
+
+		camera.update();
+	}
+
+	private void clearScreen(float r, float g, float b) {
+		Gdx.gl.glClearColor(r, g, b, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
--- desktop/src/fi/tuni/atomics/desktop/DesktopLauncher.java
+++ desktop/src/fi/tuni/atomics/desktop/DesktopLauncher.java
@@ -8,5 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Game(), config);
+		config.width = 1200;
+		config.height = 1200;
 	}
 }
