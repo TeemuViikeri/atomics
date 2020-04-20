6e0a3eb0edc3cd1457804555e93e23adac6ea348
LeeviKu <leevi.kukkula@gmail.com>
2020-04-20 14:22:25 +0300

    trailer

--- desktop/src/fi/tuni/atomics/desktop/DesktopLauncher.java
+++ desktop/src/fi/tuni/atomics/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Atomics(), config);
-		config.width = 1334;
-		config.height = 750;
+		config.width = 1920;
+		config.height = 1080;
 		config.x = 0;
 		config.y = 0;
 	}
